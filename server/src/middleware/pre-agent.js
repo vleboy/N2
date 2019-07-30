617bf6c89fc03d4febc9826d1f1580c49373d476
cheneyweb <457299596@qq.com>
2019-07-30 14:27:36 +0800

    增加代理更新

--- server/src/middleware/pre-agent.js
+++ server/src/middleware/pre-agent.js
@@ -10,7 +10,7 @@ const log = require('tracer').colorConsole({ level: config.log.level })
 
 /**
  * 创建代理
- * 自动设置userId
+ * 自动设置id
  * 自动设置status
  * 自动设置level
  * 自动设置createAt
@@ -20,22 +20,37 @@ router.post('/agent/create', async (ctx, next) => {
     let inparam = ctx.request.body
     let mongodb = global.mongodb
     // 查询上级代理
-    let parent = inparam.parentId ? await mongodb.findOne('agent', { userId: inparam.parentId }) : {}
-    inparam.userId = _.random(999999)
+    let parent = inparam.parentId ? await mongodb.findOne('agent', { id: inparam.parentId }) : {}
+    inparam.id = _.random(999999)
     inparam.status = 1
-    inparam.parentId = parent.userId || 0
+    inparam.parentId = parent.id || 0
     inparam.parentName = parent.userName || ''
     inparam.level = parent.level + 1 || 0
-    inparam.levelIndex = parent.levelIndex ? `${parent.levelIndex},${inparam.userId}` : ''
+    inparam.levelIndex = parent.levelIndex ? `${parent.levelIndex},${inparam.id}` : inparam.id
     inparam.createAt = Date.now()
-
+    // 入参检查
     if (!inparam.userName || !inparam.userPwd || !inparam.userNick) {
         ctx.body = { err: true, res: '请检查入参' }
-    } else if (await mongodb.findOne('agent', { $or: [{ userId: inparam.userId }, { userName: inparam.userName }, { userNick: inparam.userNick }] })) {
+    } else if (await mongodb.findOne('agent', { $or: [{ id: inparam.id }, { userName: inparam.userName }, { userNick: inparam.userNick }] })) {
         ctx.body = { err: true, res: '帐号已存在' }
     } else {
         return next()
     }
 })
+
+/**
+ * 更新代理
+ */
+router.post('/agent/update', async (ctx, next) => {
+    return next()
+})
+
+/**
+ * 查询代理
+ */
+router.post('/agent/query', async (ctx, next) => {
+
+})
+
 
 module.exports = router
