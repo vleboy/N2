3b380f9589e2c133fb9df6e2d9d28f1a40148b4c
xj <799137494@qq.com>
2019-07-31 12:18:38 +0800

    接口修正

--- server/src/controller/agent.js
+++ server/src/controller/agent.js
@@ -7,7 +7,7 @@ const router = new Router()
 /**
  * 代理登陆接口
  */
-router.post('/agent/login', async (ctx, next) => {
+router.post('/login', async (ctx, next) => {
     let inparam = ctx.request.body
     let mongodb = global.mongodb
     let agentInfo = await mongodb.findOne('agent', { userName: inparam.userName })
@@ -30,21 +30,21 @@ router.post('/agent/login', async (ctx, next) => {
 /**
  * 代理加减点操作
  */
-router.post('/agent/handlerPoint', async (ctx, next) => {
+router.post('/handlerPoint', async (ctx, next) => {
 
 })
 
 /**
  * 启用/停用代理
  */
-router.post('/agent/handlerStatus', async (ctx, next) => {
+router.post('/handlerStatus', async (ctx, next) => {
 
 })
 
 /**
  * 代理流水查询
  */
-router.post('/agent/queryBill', async (ctx, next) => {
+router.post('/queryBill', async (ctx, next) => {
 
 })
 
--- server/src/controller/system.js
+++ server/src/controller/system.js
@@ -5,52 +5,61 @@ const router = new Router()
 /**
  * 创建角色接口
  */
-router.post('/system/createRole',async (ctx,next)=>{
-
+router.post('/createRole', async (ctx, next) => {
+    let inparam = ctx.request.body
+    let mongodb = global.mongodb
+    if (!inparam.roleName || !inparam.permission) {
+        ctx.body = { err: true, res: '请检查入参' }
+    } else if (await mongodb.findOne('role', { name: inparam.roleName })) {
+        ctx.body = { err: true, res: '角色已存在' }
+    } else {
+        inparam.createAt = Date.now()
+        let result = await mongodb.insert('role', inparam)
+        ctx.body = { err: false, res: result.insertedId }
+    }
 })
 
 /**
  * 删除角色接口
  */
-router.post('/system/delRole',async (ctx,next)=>{
-    
+router.post('/delRole', async (ctx, next) => {
+
 })
 
 /**
  * 创建消息接口
  */
-router.post('/system/createMessage',async (ctx,next)=>{
-    
+router.post('/createMessage', async (ctx, next) => {
+
 })
 
 /**
  * 删除消息接口
  */
-router.post('/system/delMessage',async (ctx,next)=>{
-    
+router.post('/delMessage', async (ctx, next) => {
+
 })
 
 /**
  * 查询消息列表接口
  */
-router.post('/system/messageList',async (ctx,next)=>{
-    
+router.post('/messageList', async (ctx, next) => {
+
 })
 
 /**
  * 查询充值/提现接口
  */
-router.post('/system/queryReviewList',async (ctx,next)=>{
-    
+router.post('/queryReviewList', async (ctx, next) => {
+
 })
 
 /**
  * 审核充值/提现接口
  */
-router.post('/system/handlerReview',async (ctx,next)=>{
-    
-})
+router.post('/handlerReview', async (ctx, next) => {
 
+})
 
 
 module.exports = router
