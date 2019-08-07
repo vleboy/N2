00ec6e0011be392da9aae9c4179bd71a066d92e3
xj <799137494@qq.com>
2019-08-07 10:31:19 +0800

    代理玩家的更新注册

--- server/src/middleware/pre-agent.js
+++ server/src/middleware/pre-agent.js
@@ -5,7 +5,7 @@ const Router = require('koa-router')
 const router = new Router()
 // 工具相关
 const _ = require('lodash')
-const { CheckType } = require('../util/util')
+const Util = require('../util/util')
 // 日志相关
 const log = require('tracer').colorConsole({ level: config.log.level })
 
@@ -18,28 +18,32 @@ router.post('/agent/create', async (ctx, next) => {
     let mongodb = global.mongodb
     // 入参检查
     if (!inparam.userName || !inparam.userPwd || !inparam.userNick) {
-        ctx.body = { err: true, res: '请检查入参' }
-    } else if (await mongodb.collection('agent').findOne({ $or: [{ userName: inparam.userName }, { userNick: inparam.userNick }] })) {
-        ctx.body = { err: true, res: '帐号/昵称已存在' }
-    } else {
-        // 查询上级代理
-        let parent = inparam.parentId ? await mongodb.collection('agent').findOne( { id: inparam.parentId }) : {}
-        let flag = true
-        while (flag) {
-            inparam.id = _.random(100000, 999999)
-            if (!await mongodb.collection('agent').findOne( { id: inparam.id })) {
-                flag = false
-            }
-        }
-        inparam.status = 1
-        inparam.role = 'agent'
-        inparam.parentId = parent.id || 0
-        inparam.level = parent.level + 1 || 0
-        inparam.parentName = parent.userName || 'system'
-        inparam.levelIndex = parent.levelIndex ? `${parent.levelIndex},${inparam.id}` : inparam.id.toString()
-        inparam.createAt = Date.now()
-        return next()
+        return ctx.body = { err: true, res: '请检查入参' }
     }
+    if (inparam.userName.length < 3 || inparam.userName.length > 20 || inparam.userPwd.length < 6 || inparam.userPwd.length > 20 || inparam.userNick.length < 3 || inparam.userNick.length > 20) {
+        return ctx.body = { err: true, res: '参数不合法' }
+    }
+    if (await mongodb.collection(Util.CollectionEnum.agent).findOne({ $or: [{ userName: inparam.userName }, { userNick: inparam.userNick }] })) {
+        return ctx.body = { err: true, res: '帐号/昵称已存在' }
+    }
+    // 查询上级代理
+    let parent = inparam.parentId ? await mongodb.collection(Util.CollectionEnum.agent).findOne({ id: inparam.parentId }) : {}
+    let flag = true
+    while (flag) {
+        inparam.id = _.random(100000, 999999)
+        if (!await mongodb.collection(Util.CollectionEnum.agent).findOne({ id: inparam.id })) {
+            flag = false
+        }
+    }
+    inparam.status = Util.StatusEnum.Enable
+    inparam.role = Util.RoleEnum.agent
+    inparam.parentId = parent.id || 0
+    inparam.level = parent.level + 1 || 0
+    inparam.parentName = parent.userName || 'system'
+    inparam.levelIndex = parent.levelIndex ? `${parent.levelIndex},${inparam.id}` : inparam.id.toString()
+    inparam.createAt = Date.now()
+    return next()
+
 
 })
 
@@ -50,19 +54,23 @@ router.post('/agent/update', async (ctx, next) => {
     const token = ctx.tokenVerify
     let inparam = ctx.request.body
     let mongodb = global.mongodb
-    // inparam = _.pick(inparam, ['userPwd', 'userNick', 'gameList', 'status'])
+    //只允许更新的参数
+    let diffArr = _.difference(Object.keys(inparam), ['userPwd', 'userNick', 'gameList', 'status'])
+    if (diffArr.length > 0) {
+        return ctx.body = { err: true, res: `以下参数不能更新${diffArr.join(',')}` }
+    }
     let agentInfo = ''
     if (inparam.userPwd && (inparam.userPwd.length < 6 || inparam.userPwd.length > 20)) {
         ctx.body = { err: true, res: '密码长度不合法' }
     } else if (inparam.userNick && (inparam.userNick.length < 3 || inparam.userNick.length > 20)) {
         ctx.body = { err: true, res: '昵称长度不合法' }
-    } else if (inparam.gameList && (CheckType(inparam.gameList) != 'array')) {
+    } else if (inparam.gameList && (Util.CheckType(inparam.gameList) != 'array')) {
         ctx.body = { err: true, res: '游戏列表不合法' }
-    } else if (!(agentInfo = await mongodb.collection('agent').findOne({ id: inparam.id }))) {
+    } else if (!(agentInfo = await mongodb.collection(Util.CollectionEnum.agent).findOne({ id: inparam.id }))) {
         ctx.body = { err: true, res: '代理不存在' }
     } else {
-        if (inparam.status == 0 || inparam.status == 1) {
-            if (token.id != agentInfo.parentId) {
+        if (inparam.status == Util.StatusEnum.Disable || inparam.status == Util.StatusEnum.Enable) {
+            if (token.role == Util.RoleEnum.agent && token.id != agentInfo.parentId) {
                 return ctx.body = { err: true, res: '不能越级操作' }
             }
         }
