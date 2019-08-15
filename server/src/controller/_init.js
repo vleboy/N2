e72c26bf3cbaaa3de309f2f4dc85889292001803
cheneyweb <457299596@qq.com>
2019-08-15 10:28:03 +0800

    增加预置数据

--- server/src/controller/_init.js
+++ server/src/controller/_init.js
@@ -22,20 +22,24 @@ router.post('/init', async (ctx, next) => {
         await mongodb.collection(Util.CollectionEnum.message).createIndex({ id: -1 })
         // 预置数据
         await mongodb.collection(Util.CollectionEnum._seq).insertMany([{ seqName: 'billSeq', seqValue: 0 }, { seqName: 'reviewSeq', seqValue: 0 }, { seqName: 'messageSeq', seqValue: 0 }])
+
         await mongodb.collection(Util.CollectionEnum.agent).insertOne({ id: 100000, role: 'admin', userName: 'admin', userPwd: '123456', userNick: '超级管理员', subrole: '超级管理员', status: 1, createAt, createAtStr: moment(createAt).utcOffset(8).format('YYYY-MM-DD HH:mm:ss') })
         await mongodb.collection(Util.CollectionEnum.subrole).insertOne({ id: 1000, roleName: '超级管理员', permissions: ['所有权限', '代理中心', '玩家中心', '代理账单', '玩家账单', '审核中心', '管理中心', '管理员列表', '角色列表'], createAt, createAtStr: moment(createAt).utcOffset(8).format('YYYY-MM-DD HH:mm:ss') })
-        await mongodb.collection(Util.CollectionEnum.config).insertOne({ code: 'commission', name: '佣金比', value: 0.008 })
-        await mongodb.collection(Util.CollectionEnum.config).insertOne({ code: 'deposit', name: '存款手续费', value: 0.02 })
-        await mongodb.collection(Util.CollectionEnum.config).insertOne({ code: 'withdraw', name: '取款手续费', value: 0.01 })
-        await mongodb.collection(Util.CollectionEnum.config).insertOne({ code: '70000', name: 'NA电子平台费', value: 0.06 })
-        await mongodb.collection(Util.CollectionEnum.config).insertOne({ code: '10300000', name: 'MG电子平台费', value: 0.06 })
-        await mongodb.collection(Util.CollectionEnum.config).insertOne({ code: '1080000', name: 'SUN电子平台费', value: 0.06 })
-        await mongodb.collection(Util.CollectionEnum.config).insertOne({ code: '1160000', name: 'PP电子平台费', value: 0.06 })
-        await mongodb.collection(Util.CollectionEnum.config).insertOne({ code: '1050000', name: 'AG真人平台费', value: 0.06 })
-        await mongodb.collection(Util.CollectionEnum.config).insertOne({ code: '1120000', name: 'SUN真人平台费', value: 0.06 })
-        await mongodb.collection(Util.CollectionEnum.config).insertOne({ code: '1130000', name: 'YSB体育平台费', value: 0.06 })
-        await mongodb.collection(Util.CollectionEnum.config).insertOne({ code: '1170000', name: 'NA电竞平台费', value: 0.06 })
-        await mongodb.collection(Util.CollectionEnum.config).insertOne({ code: '1100000', name: 'VG棋牌平台费', value: 0.06 })
+
+        await mongodb.collection(Util.CollectionEnum.config).insertOne({ code: 'commission', name: '佣金比(百分比)', value: 0.8 })
+        await mongodb.collection(Util.CollectionEnum.config).insertOne({ code: 'ratio', name: '占成比(百分比)', value: 15 })
+        await mongodb.collection(Util.CollectionEnum.config).insertOne({ code: 'deposit', name: '存款手续费(百分比)', value: 2 })
+        await mongodb.collection(Util.CollectionEnum.config).insertOne({ code: 'withdraw', name: '取款手续费(百分比)', value: 1 })
+
+        await mongodb.collection(Util.CollectionEnum.config).insertOne({ code: '70000', name: 'NA电子平台费(百分比)', value: 6 })
+        await mongodb.collection(Util.CollectionEnum.config).insertOne({ code: '10300000', name: 'MG电子平台费(百分比)', value: 6 })
+        await mongodb.collection(Util.CollectionEnum.config).insertOne({ code: '1080000', name: 'SUN电子平台费(百分比)', value: 6 })
+        await mongodb.collection(Util.CollectionEnum.config).insertOne({ code: '1160000', name: 'PP电子平台费(百分比)', value: 6 })
+        await mongodb.collection(Util.CollectionEnum.config).insertOne({ code: '1050000', name: 'AG真人平台费(百分比)', value: 6 })
+        await mongodb.collection(Util.CollectionEnum.config).insertOne({ code: '1120000', name: 'SUN真人平台费(百分比)', value: 6 })
+        await mongodb.collection(Util.CollectionEnum.config).insertOne({ code: '1130000', name: 'YSB体育平台费(百分比)', value: 6 })
+        await mongodb.collection(Util.CollectionEnum.config).insertOne({ code: '1170000', name: 'NA电竞平台费(百分比)', value: 6 })
+        await mongodb.collection(Util.CollectionEnum.config).insertOne({ code: '1100000', name: 'VG棋牌平台费(百分比)', value: 6 })
 
         ctx.body = 'Y'
     } else {
