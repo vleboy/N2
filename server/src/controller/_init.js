8aa575e98e4412de4ab9b80e18640a0a516b6f2a
cheneyweb <457299596@qq.com>
2019-08-15 10:22:15 +0800

    增加预置数据

--- server/src/controller/_init.js
+++ server/src/controller/_init.js
@@ -25,6 +25,8 @@ router.post('/init', async (ctx, next) => {
         await mongodb.collection(Util.CollectionEnum.agent).insertOne({ id: 100000, role: 'admin', userName: 'admin', userPwd: '123456', userNick: '超级管理员', subrole: '超级管理员', status: 1, createAt, createAtStr: moment(createAt).utcOffset(8).format('YYYY-MM-DD HH:mm:ss') })
         await mongodb.collection(Util.CollectionEnum.subrole).insertOne({ id: 1000, roleName: '超级管理员', permissions: ['所有权限', '代理中心', '玩家中心', '代理账单', '玩家账单', '审核中心', '管理中心', '管理员列表', '角色列表'], createAt, createAtStr: moment(createAt).utcOffset(8).format('YYYY-MM-DD HH:mm:ss') })
         await mongodb.collection(Util.CollectionEnum.config).insertOne({ code: 'commission', name: '佣金比', value: 0.008 })
+        await mongodb.collection(Util.CollectionEnum.config).insertOne({ code: 'deposit', name: '存款手续费', value: 0.02 })
+        await mongodb.collection(Util.CollectionEnum.config).insertOne({ code: 'withdraw', name: '取款手续费', value: 0.01 })
         await mongodb.collection(Util.CollectionEnum.config).insertOne({ code: '70000', name: 'NA电子平台费', value: 0.06 })
         await mongodb.collection(Util.CollectionEnum.config).insertOne({ code: '10300000', name: 'MG电子平台费', value: 0.06 })
         await mongodb.collection(Util.CollectionEnum.config).insertOne({ code: '1080000', name: 'SUN电子平台费', value: 0.06 })
