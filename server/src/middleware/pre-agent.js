cbde714aa7d4d22c9c585941995bad7f04b8cf1f
cheneyweb <457299596@qq.com>
2019-08-20 15:16:09 +0800

    修改代理增加可选参数

--- server/src/middleware/pre-agent.js
+++ server/src/middleware/pre-agent.js
@@ -66,7 +66,7 @@ router.post('/agent/update', async (ctx, next) => {
     //     return ctx.body = { err: true, res: '请检查入参' }
     // }
     //只允许更新的参数
-    let diffArr = _.difference(Object.keys(inparam), ['id', 'oldPwd', 'userPwd', 'subrole', 'status', 'gameList'])
+    let diffArr = _.difference(Object.keys(inparam), ['id', 'oldPwd', 'userPwd', 'subrole', 'status', 'gameList', 'mobile', 'email'])
     if (diffArr.length > 0) {
         return ctx.body = { err: true, res: `以下参数不能更新【${diffArr.join(',')}】` }
     }
