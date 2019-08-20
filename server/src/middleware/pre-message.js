4113be48527c997ad57291b31828fb91c7200243
cheneyweb <457299596@qq.com>
2019-08-20 09:53:08 +0800

    消息查询

--- server/src/middleware/pre-message.js
+++ server/src/middleware/pre-message.js
@@ -67,6 +67,8 @@ router.get('/message/query', async (ctx, next) => {
     const token = ctx.tokenVerify
     if (inparam.project == Util.MsgEnum.Private) {
         inparam.ownerId = token.id
+    } else {
+        inparam.project = { $or: [{ project: Util.MsgEnum.NoticeAll }, { project: Util.MsgEnum.NoticeAgent }] }
     }
     return next()
 })
