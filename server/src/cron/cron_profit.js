43332ac21d20ceb99e557076d48ccf89b4182f83
cheneyweb <457299596@qq.com>
2019-08-22 16:53:49 +0800

    调整字段

--- server/src/cron/cron_profit.js
+++ server/src/cron/cron_profit.js
@@ -22,12 +22,12 @@ cron.schedule('0 1 0 1 * *', async () => {
     //获取所有代理
     let agents = await global.mongodb.collection(Util.CollectionEnum.agent).find({ role: Util.RoleEnum.agent }, { projection: { id: 1, role: 1, userName: 1, userNick: 1, mode: 1, modeValue: 1, _id: 0 } }).toArray()
     for (let agent of agents) {
-        currentProfit(agent, configArr, startTime, endTime, month)
+        profit(agent, configArr, startTime, endTime, month)
     }
 })
 
 //净利润
-async function currentProfit(agent, configArr, startTime, endTime, month) {
+async function profit(agent, configArr, startTime, endTime, month) {
     let data = {
         project: Util.ProjectEnum.Profit,     // 类型  
         role: agent.role,                     // 角色
@@ -60,39 +60,39 @@ async function currentProfit(agent, configArr, startTime, endTime, month) {
         // 当局输赢
         let roundWinloseAmount = +round.winloseAmount.toFixed(2)
         // 累计输赢
-        data.currentWinlose = NP.plus(data.currentWinlose, roundWinloseAmount)
+        data.winlose = NP.plus(data.winlose, roundWinloseAmount)
         // 累计有效投注
         let roundBetAmount = _.sumBy(round.bills, o => { if (o.project == Util.ProjectEnum.Bet) return Math.abs(o.amount) })
         let roundValidBetAmount = Math.min(Math.abs(+roundBetAmount.toFixed(2)), Math.abs(roundWinloseAmount))
-        data.currentCommission = NP.plus(data.currentCommission, roundValidBetAmount)
+        data.commission = NP.plus(data.commission, roundValidBetAmount)
         // 累计平台输赢
         let sourceGameId = round.sourceGameId.toString()
         let plat = `${sourceGameId.substring(0, sourceGameId.length - 2)}00`
         platFeeMap[plat] = (platFeeMap[plat] || platFeeMap[plat] == 0) ? NP.plus(platFeeMap[plat], roundWinloseAmount) : roundWinloseAmount
     }
     // 使用佣金比例计算佣金
-    data.currentCommissionFee = +(data.currentCommission * _.find(configArr, o => o.id == 'commission').value / 100).toFixed(2)
+    data.commissionFee = +(data.commission * _.find(configArr, o => o.id == 'commission').value / 100).toFixed(2)
     // 使用平台费比例计算平台费
     for (let plat in platFeeMap) {
-        data.currentPlatformFee = NP.plus(data.currentPlatformFee, +(platFeeMap[plat] * _.find(configArr, o => o.id == plat).value / 100).toFixed(2))
+        data.platformFee = NP.plus(data.platformFee, +(platFeeMap[plat] * _.find(configArr, o => o.id == plat).value / 100).toFixed(2))
     }
     // 累计玩家存取款
     for (let bill of bills) {
         if (bill.project == Util.ProjectEnum.Deposit) {
-            data.currentDeposit = NP.plus(data.currentDeposit, Math.abs(bill.amount))
+            data.deposit = NP.plus(data.deposit, Math.abs(bill.amount))
         } else {
-            data.currentWithdraw = NP.plus(data.currentWithdraw, Math.abs(bill.amount))
+            data.withdraw = NP.plus(data.withdraw, Math.abs(bill.amount))
         }
     }
     // 使用手续费比例计算存取手续费
-    data.currentDepositFee = +(data.currentDeposit * _.find(configArr, o => o.id == 'deposit').value / 100).toFixed(2)
-    data.currentWithdrawFee = +(data.currentWithdraw * _.find(configArr, o => o.id == 'withdraw').value / 100).toFixed(2)
-    data.currentWinlose *= -1           // 总输赢取反(玩家输，代理赢)
-    data.currentPlatformFee *= -1       //平台费取反（玩家输，平台赢利）
+    data.depositFee = +(data.deposit * _.find(configArr, o => o.id == 'deposit').value / 100).toFixed(2)
+    data.withdrawFee = +(data.withdraw * _.find(configArr, o => o.id == 'withdraw').value / 100).toFixed(2)
+    data.winlose *= -1           // 总输赢取反(玩家输，代理赢)
+    data.platformFee *= -1       //平台费取反（玩家输，平台赢利）
     // 当前利润（当前输赢 - 成本）* 业务模式比例
-    data.currentProfit = +((data.currentWinlose - data.currentCommissionFee - data.currentPlatformFee - data.currentDepositFee - data.currentWithdrawFee) * agent.modeValue / 100).toFixed(2)
+    data.profit = +((data.winlose - data.commissionFee - data.platformFee - data.depositFee - data.withdrawFee) * agent.modeValue / 100).toFixed(2)
     // 写入发放表
-    if (data.currentProfit > 0) {
+    if (data.profit > 0) {
         data.id = await Util.getSeq('profitSeq')   
         data.createAt = Date.now()
         data.month = month
