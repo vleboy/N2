63047d6cd92c4797a84e25870684b45e498985b5
cheneyweb <457299596@qq.com>
2019-08-23 15:37:44 +0800

    玩家APP更新依赖以及PWA

--- player/vue.config.js
+++ player/vue.config.js
@@ -1,4 +1,7 @@
+//px转rem
 module.exports = {
+  productionSourceMap: false,
+
   css: {
     loaderOptions: {
       css: {},
@@ -11,13 +14,57 @@ module.exports = {
       }
     }
   },
+
   pwa: {
+    name: 'YIBO',
+    themeColor: '#FFFFFF',
+    msTileColor: '#FFFFFF',
     iconPaths: {
       favicon32: 'favicon.ico',
       favicon16: 'favicon.ico',
       appleTouchIcon: 'favicon.ico',
       maskIcon: 'favicon.ico',
       msTileImage: 'favicon.ico'
+    },
+    workboxOptions: {
+      runtimeCaching: [{
+        urlPattern: new RegExp('^https://api.1bets.co/'),
+        handler: 'staleWhileRevalidate',
+        options: {
+          cacheName: 'api-cache',
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      }]
     }
   }
+
 }
+
+//px转vh/vw
+/* module.exports = {
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          require("postcss-px-to-viewport")({
+            unitToConvert: "px",
+            viewportWidth: 750,
+            unitPrecision: 3,
+            propList: [
+              "*"
+            ],
+            viewportUnit: "vw",
+            fontViewportUnit: "vw",
+            selectorBlackList: [],
+            minPixelValue: 1,
+            mediaQuery: false,
+            replace: true,
+            exclude: /(\/|\\)(node_modules)(\/|\\)/,
+          })
+        ]
+      }
+    }
+  }
+} */
